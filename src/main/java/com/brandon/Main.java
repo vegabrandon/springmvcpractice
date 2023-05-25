53a38b804e1825e6864c0252c61658e9c5df6eda
vegabrandon <vegabrandon1250@gmail.com>
2023-05-25 13:43:41 -0400

    Created test endpoint that works as intended

--- src/main/java/com/brandon/Main.java
+++ src/main/java/com/brandon/Main.java
@@ -2,10 +2,21 @@ package com.brandon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// Annotation required for SpringBootApps
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+
+    // Maps Get Request to Endpoint
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello";
     }
 }
