e95a9d5a9be187bea3fb065dce8a67a6c6964a55
vegabrandon <vegabrandon1250@gmail.com>
2023-05-26 09:07:26 -0400

    Added constructor for customer class

--- src/main/java/com/brandon/Customer.java
+++ src/main/java/com/brandon/Customer.java
@@ -1,4 +1,15 @@
 package com.brandon;
 
 public class Customer {
+    private Integer id;
+    private Integer age;
+    private String name;
+    private String email;
+
+    public Customer(Integer id, Integer age, String name, String email) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.email = email;
+    }
 }
