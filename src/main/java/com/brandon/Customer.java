68ac9a9c825673c9ba816d5b51ed2f91cce48642
vegabrandon <vegabrandon1250@gmail.com>
2023-05-26 09:10:12 -0400

    Getters and setters for customer class

--- src/main/java/com/brandon/Customer.java
+++ src/main/java/com/brandon/Customer.java
@@ -12,4 +12,37 @@ public class Customer {
         this.name = name;
         this.email = email;
     }
+    public Customer(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
